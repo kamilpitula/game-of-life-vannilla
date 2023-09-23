95a5a4fc8decdd326c36aeb78a0d8466e1ceb1fc
Kamil Pitula <pitula.k@gmail.com>
2023-09-23 14:33:33 +0200

    improve board creation performance pt1

--- game-of-life-vannilla/UI/board.js
+++ game-of-life-vannilla/UI/board.js
@@ -22,7 +22,14 @@ Board.prototype.handleCellStateChange = function (cell) {
 };
 
 Board.prototype.initBoard = function () {
-  this.rootEl.innerHTML = "";
+  const boardElement = document.createElement("div");
+  boardElement.id = "board";
+
+  const documentFragment = document.createDocumentFragment();
+
+  const buttonProto = document.createElement("button");
+  buttonProto.classList.add("cell__button");
+
   const board = this.game.board;
   for (let row = 0; row < board.length; row++) {
     const rowEl = document.createElement("div");
@@ -30,20 +37,23 @@ Board.prototype.initBoard = function () {
     rowEl.classList.add("board__row");
     for (let column = 0; column < board[row].length; column++) {
       const cell = board[row][column];
-      const cellButtonEl = document.createElement("button");
+      const cellButtonEl = buttonProto.cloneNode(true);
       cellButtonEl.addEventListener(
         "click",
         changeCellStateHandler.bind(cellButtonEl, this.game, cell)
       );
-      cellButtonEl.classList.add("cell__button");
-      cellButtonEl.textContent = `${cell.positionX}-${cell.positionY}`;
       if (cell.isAlive) cellButtonEl.classList.add("cell__button--alive");
       rowEl.appendChild(cellButtonEl);
       rowElements.push(cellButtonEl);
     }
-    this.rootEl.appendChild(rowEl);
+    documentFragment.appendChild(rowEl);
     this.cellElements.push(rowElements);
   }
+
+  boardElement.appendChild(documentFragment);
+  
+  this.rootEl.replaceWith(boardElement);
+  this.rootEl = boardElement;
 };
 
 export { Board };
